daa853b0dde53bf53f79a94b9ebea3c897044cec
fgilm16@informatica.iesvalledeljerteplasencia.es <fgilm16@informatica.iesvalledeljerteplasencia.es>
2025-10-01 19:48:40 +0200

    Ejercicio 307: 307Persona Crea una clase Persona que sea padre de Empleado

---
+++ Tema1/3 PHP orientado a Objetos/307/307Persona.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+
+        class Persona {
+
+            /**
+             * @param $nombre
+             */
+            public function __construct(
+                protected string $nombre, 
+                protected string $apellido, 
+                ) {}
+
+        }
+        
+        class Empleado extends Persona {            
+            private array $telefonos = [];
+            public static int $sueldoTope = 3333;
+
+            /**
+             * @param $nombre
+             */
+            public function __construct(string $nombre, string $apellido, private int $sueldo = 1000) {
+                parent::__construct($nombre, $apellido);
+            }
+
+            // Setter
+            public function setSueldoTope(int $sueldoTope): void {
+                self::$sueldoTope = $sueldoTope;
+            }
+
+            public function getSueldoTope(): int {
+                return self::$sueldoTope;
+            }
+
+            public function debePagarImpuestos(): bool {
+                return  $this->sueldo > self::$sueldoTope;
+            }
+
+            function anyadirTelefono(int $telefono) : void {
+                array_push($this->telefonos, $telefono);
+            }
+
+            // Opcion 1
+            public function listarTelefonos(): string {
+                if (empty($this->telefonos)) {
+                    return '';
+                }
+
+                $todosTelefonos = '';
+                foreach ($this->telefonos as $telefono) {
+                    $todosTelefonos .= $telefono . ', ';
+                }
+
+                return rtrim($todosTelefonos, ', ');
+            }
+
+            // Opcion 2: Con implode
+            public function listarTelefonos2(): string {
+                return implode(", ", $this->telefonos);
+            }
+
+            public function vaciarTelefonos(): void {
+                $this->telefonos = [];
+            }
+
+            public function __toString() {
+                return "Empleado{" . "nombre='" . $this->nombre . "', " . "apellidos='" . $this->apellido . "'}";
+            }
+
+            public static function toHtml(Empleado $emp): string {
+                $telefonos = $emp->telefonos;
+                
+                $html = '<h1>'. $emp->__toString() . '</h1><br>' . 
+                '<ol>';
+                foreach ($telefonos as $telefono) {
+                    $html .= '<li>' . $telefono . '</li>';
+                }
+                $html .= '</ol>';
+
+                return $html;
+            }
+
+
+        }
+
+        $empleado1 = new Empleado("Francisco", "Gil", 6500);
+        echo $empleado1->toHtml($empleado1);
+    ?>
+
+
+</body>
+</html>
