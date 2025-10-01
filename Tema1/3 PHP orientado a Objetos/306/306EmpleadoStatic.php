451b964e56f255ac9b9b2d70cb1c00d3808cd7d2
fgilm16@informatica.iesvalledeljerteplasencia.es <fgilm16@informatica.iesvalledeljerteplasencia.es>
2025-10-01 19:27:45 +0200

    Ejercicio 306: 306EmpleadoStatic Completa el siguiente método con una cadena HTML que muestre los datos de un empleado dentro de un párrafo y todos los teléfonos mediante una lista ordenada

---
+++ Tema1/3 PHP orientado a Objetos/306/306EmpleadoStatic.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+
+        
+        class Empleado {
+            /**
+             * En PHP 8 no es necesario declarar cada atributo de la clase
+             */
+            // private string $nombre;
+            // private string $apellido;
+            // private int $sueldo;
+            private array $telefonos = [];
+            public static int $sueldoTope = 3333;
+
+            /**
+             * @param $nombre
+             */
+            public function __construct(
+                private string $nombre, 
+                private string $apellido, 
+                private int $sueldo = 1000) {
+                /**
+                 * En PHP 8 no es necesario asignar los valores al instanciar una clase
+                 */
+                // $this->nombre = $nombre;
+                // $this->apellido = $apellido;
+                // $this->sueldo = $sueldo;
+            }
+
+
+            // Setter
+            public function setSueldoTope(int $sueldoTope): void {
+                self::$sueldoTope = $sueldoTope;
+            }
+
+            public function getSueldoTope(): int {
+                return self::$sueldoTope;
+            }
+
+            public function debePagarImpuestos(): bool {
+                return  $this->sueldo > self::$sueldoTope;
+            }
+
+            function anyadirTelefono(int $telefono) : void {
+                array_push($this->telefonos, $telefono);
+            }
+
+            // Opcion 1
+            public function listarTelefonos(): string {
+                if (empty($this->telefonos)) {
+                    return '';
+                }
+
+                $todosTelefonos = '';
+                foreach ($this->telefonos as $telefono) {
+                    $todosTelefonos .= $telefono . ', ';
+                }
+
+                return rtrim($todosTelefonos, ', ');
+            }
+
+            // Opcion 2: Con implode
+            public function listarTelefonos2(): string {
+                return implode(", ", $this->telefonos);
+            }
+
+            public function vaciarTelefonos(): void {
+                $this->telefonos = [];
+            }
+
+            public function __toString() {
+                return "Empleado{" . "nombre='" . $this->nombre . "', " . "apellidos='" . $this->apellido . "'}";
+            }
+
+            public static function toHtml(Empleado $emp): string {
+                $telefonos = $emp->telefonos;
+
+                var_dump($telefonos);
+                $html = '<h1>'. $emp->__toString() . '</h1><br>' . 
+                '<ol>';
+
+                foreach ($telefonos as $telefono) {
+                    $html .= '<li>' . $telefono . '</li>';
+                }
+
+                $html .= '</ol>';
+
+                var_dump($html);
+
+                return $html;
+            }
+
+
+        }
+
+
+        $empleado1 = new Empleado("Francisco", "Gil", 6500);
+
+        $pagarImpuestos = $empleado1->debePagarImpuestos() ? "Debe pagar impuestos" : "No Debe pagar impuestos";
+        
+
+        $empleado1->anyadirTelefono(606987487);
+        $empleado1->anyadirTelefono(607577488);
+
+        echo "<br>Teléfonos con forearch: " . $empleado1->listarTelefonos();
+        echo "<br>Teléfonos con implode: " . $empleado1->listarTelefonos2();
+    
+        // $empleado1->vaciarTelefonos();
+        echo "<br>";
+        // $empleado1->listarTelefonos();
+
+
+        echo "<br> Impuestos";
+        $empleado2 = new Empleado("Javier", "Santos");
+        var_dump($empleado2);
+        echo $empleado2->debePagarImpuestos() ? "Debe pagar impuestos" : "No Debe pagar impuestos";
+        
+
+        $empleado2->setSueldoTope(1658);
+
+        echo "<br>";
+        echo "El sueldo tope del empleado 2 es: " . $empleado2->getSueldoTope();
+
+        echo "<br>";
+        // echo $empleado2;
+
+        echo $empleado2->toHtml($empleado1);
+    ?>
+
+
+</body>
+</html>
