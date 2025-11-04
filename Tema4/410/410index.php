8cb4641c71308523c6c3d942d6a7a36d36791e89
Timba <fcogilmerino@gmail.com>
2025-11-04 07:56:15 +0100

    Ejercicio 410: 410index.php: formulario de inicio de sesión. WIP

---
+++ Tema4/410/410index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario de inicio de sesión</title>
+</head>
+<body>
+    <form action="411login.php" method="post">
+        
+    </form>
+</body>
+</html>
+
+
